7002026094950df15551afa354e19324b5dd5c7a
cclauss <cclauss@bluewin.ch>
2016-05-02 05:15:06 +0200

    An alternate version of sudoku.py to consider (#20)
    * An alternate version of sudoku.py to consider
    
    * Why create a Sudoku object when is all work is done in the `__init__()` method?
    * Why have getImagePath() method that merely encapsulates os.path.abspath()?
    * Why not create `res` on-the-fly (list comprehension or generator function)?
    * create_net() takes a rel_path to enable benchmarks between different nets.
    * Provide usage info to command line
    
    * Update sudoku_alt.py
    
    * image_name --> image_path

---
+++ sudoku_alt.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import pickle
+import sys
+
+import numpy as np
+
+from scripts.sudokuExtractor import Extractor
+from scripts.train import NeuralNetwork
+from scripts.sudoku_str import SudokuStr
+
+
+def create_net(rel_path):
+    with open(os.getcwd() + rel_path) as in_file:
+        sizes, biases, wts = pickle.load(in_file)
+    return NeuralNetwork(customValues=(sizes, biases, wts))
+
+
+def get_cells(image_path):  # yields 9 * 9 = 81 cells
+    net = create_net(rel_path='/networks/net')
+    for cell in Extractor(os.path.abspath(image_path)).cells:
+        x = net.feedforward(np.reshape(cell, (784, 1)))
+        x[0] = 0
+        digit = np.argmax(x)
+        yield str(digit) if list(x[digit])[0] / sum(x) > 0.8 else ' '
+
+
+def snap_sudoku(image_path):
+    s = SudokuStr(''.join(cell for cell in get_cells(image_path)))
+    print(s)
+    try:
+        print('\nSolving...\n\n{}'.format(s.solve()))
+    except ValueError:
+        print('No solution found.  Please rescan the puzzle.')
+
+
+if __name__ == '__main__':
+    try:
+        snap_sudoku(image_path=sys.argv[1])
+    except IndexError:
+        fmt = 'usage: {} image_path'
+        print(fmt.format(__file__.split('/')[-1]))
