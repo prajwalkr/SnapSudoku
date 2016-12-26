a36d9cb1a9f78280c5cda655c6cb9f082b7ed490
Yash Mittal <yashmittal2009@gmail.com>
2016-12-26 13:34:23 +0530

    Experimenting http post with image

--- android/app/src/main/java/com/example/yash/snapsudoku/MainActivity.java
+++ android/app/src/main/java/com/example/yash/snapsudoku/MainActivity.java
@@ -1,16 +1,29 @@
 package com.example.yash.snapsudoku;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.AnyRes;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.example.yash.snapsudoku.network.SudokuService;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
@@ -26,17 +39,61 @@ public class MainActivity extends AppCompatActivity {
                 .build();
 
         final SudokuService service = retrofit.create(SudokuService.class);
-        service.getIndex()
-                .enqueue(new Callback<String>() {
-                    @Override
-                    public void onResponse(Call<String> call, Response<String> response) {
-                        Toast.makeText(MainActivity.this, response.body().toString(), Toast.LENGTH_SHORT).show();
-                    }
+//        service.getIndex()
+//                .enqueue(new Callback<String>() {
+//                    @Override
+//                    public void onResponse(Call<String> call, Response<String> response) {
+//                        Toast.makeText(MainActivity.this, response.body().toString(), Toast.LENGTH_SHORT).show();
+//                    }
+//
+//                    @Override
+//                    public void onFailure(Call<String> call, Throwable t) {
+//                        Toast.makeText(MainActivity.this, t.getMessage().toString(), Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+//
+//        Uri fileUri = getUriToDrawable(this, R.drawable.test2);
 
-                    @Override
-                    public void onFailure(Call<String> call, Throwable t) {
-                        Toast.makeText(MainActivity.this, t.getMessage().toString(), Toast.LENGTH_SHORT).show();
-                    }
-                });
+        Drawable drawable = getResources().getDrawable(R.drawable.test2);
+
+        Uri fileUri = Uri.parse("android.resource://" + this.getPackageName() + "/drawable/test2.jpg");
+        File file = new File("drawable://" + R.drawable.test2);
+
+        Log.d("LOG_TAG", file.getPath());
+
+        if (file.exists()) {
+            RequestBody requestBody = RequestBody.create(MediaType.parse("*/*"), file);
+            MultipartBody.Part image = MultipartBody.Part.createFormData("image", file.getName(), requestBody);
+
+            service.solveSudoku(image)
+                    .enqueue(new Callback<String>() {
+                        @Override
+                        public void onResponse(Call<String> call, Response<String> response) {
+                            Toast.makeText(MainActivity.this, "Right", Toast.LENGTH_SHORT).show();
+                        }
+
+                        @Override
+                        public void onFailure(Call<String> call, Throwable t) {
+                            Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    });
+        } else {
+            Toast.makeText(this, "File does not exist", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    /*
+    * get uri to drawable or any other resource type if u wish
+    * @param context - context
+    * @param drawableId - drawable res id
+    * @return - uri
+    */
+    public static final Uri getUriToDrawable(@NonNull Context context, @AnyRes int drawableId) {
+        Uri imageUri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE +
+                "://" + context.getResources().getResourcePackageName(drawableId)
+                + '/' + context.getResources().getResourceTypeName(drawableId)
+                + '/' + context.getResources().getResourceEntryName(drawableId));
+        return imageUri;
     }
 }
--- android/app/src/main/java/com/example/yash/snapsudoku/network/SudokuService.java
+++ android/app/src/main/java/com/example/yash/snapsudoku/network/SudokuService.java
@@ -1,7 +1,11 @@
 package com.example.yash.snapsudoku.network;
 
+import okhttp3.MultipartBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
 import retrofit2.http.Query;
 
 /**
@@ -11,4 +15,8 @@ public interface SudokuService {
 
     @GET(".")
     Call<String> getIndex();
+
+    @Multipart
+    @POST(" sudoku/solve/")
+    Call<String> solveSudoku(@Part MultipartBody.Part image);
 }
